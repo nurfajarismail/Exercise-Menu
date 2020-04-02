6213728acf9cbc2e5750b673f60affc64fcd7c0a
Nur Fajar Ismail <nur.fajar.ft18@mail.umy.ac.id>
2020-04-02 22:54:33 +0700

    add code if di mainActivity

--- app/src/main/java/com/example/exercise_menu/MainActivity.java
+++ app/src/main/java/com/example/exercise_menu/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.exercise_menu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button blog;
+    EditText nama,sandi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        blog=(Button)findViewById(R.id.btnok);
+        nama= (EditText) findViewById(R.id.editname);
+        sandi= (EditText) findViewById(R.id.editPass);
+        blog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(nama.getText().toString().equals("admin") &&
+                sandi.getText().toString().equals("123")){
+                    Toast.makeText(getApplicationContext(),"bisa sign in",Toast.LENGTH_SHORT).show();
+                }else{
+
+                }
+            }
+        });
+
+
+
+
+
     }
 }
