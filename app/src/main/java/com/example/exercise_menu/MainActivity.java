a8c398e3b73a128977722fb715c6904b57dab73d
Nur Fajar Ismail <nur.fajar.ft18@mail.umy.ac.id>
2020-04-03 02:26:18 +0700

    add allert dialog

--- app/src/main/java/com/example/exercise_menu/MainActivity.java
+++ app/src/main/java/com/example/exercise_menu/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.exercise_menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if(nama.getText().toString().equals("admin") &&
                 sandi.getText().toString().equals("123")){
+                    startActivity(new Intent(MainActivity.this, main2.class));
                     Toast.makeText(getApplicationContext(),"bisa sign in",Toast.LENGTH_SHORT).show();
                 }else{
                     Toast.makeText(getApplicationContext(),"Password Salah",Toast.LENGTH_SHORT).show();
---
+++ app/src/main/java/com/example/exercise_menu/main2.java
@@ -0,0 +1,65 @@
+package com.example.exercise_menu;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class main2 extends AppCompatActivity {
+    private Context context;
+    TextView persegi, lingkran,pp,segitiga,trapesium;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main2);
+
+        persegi=(TextView) findViewById(R.id.persegii);
+        persegi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+
+
+        });
+
+
+
+    }
+
+    private void showDialog(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                this);
+
+        // set title dialog
+
+
+        // set pesan dari dialog
+        String[] animals = {"Hitung Luas", "Hitung Keliling",};
+        alertDialogBuilder.setItems(animals, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0:
+                    case 1:
+
+                }
+            }
+        });
+
+        // membuat alert dialog dari builder
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // menampilkan alert dialog
+        alertDialog.show();
+    }
+
+}
+
